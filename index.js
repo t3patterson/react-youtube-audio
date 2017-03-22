7f246b4e5cc235baf7a5e7d42d4091654ab57fc7
Travis Hubbard <t3patterson@gmail.com>
2017-03-22 12:12:23 -0400

    broken link

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Player = require("./lib-dist/player.js")
-module.exports  = require("./lib-dist/player.js").default
+const Player = require("./_dist/player.js").default
+module.exports  = Player
 
 // IF YOU WANT TO MODIFY (and you have es6), UNCOMMENT
 //module.exports = require("./lib/player.js")
